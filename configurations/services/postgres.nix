1d72cd8195ab7408f401d58012c1214bcaa8d1a7
Clansty <i@gao4.pw>
2022-09-11 16:12:24 +0800

    update

--- configurations/services/postgres.nix
+++ configurations/services/postgres.nix
@@ -6,6 +6,12 @@
       enable = true;
       package = pkgs.postgresql_14;
       enableTCPIP = true;
+      authentication = ''
+        local all all trust
+        host all all 127.0.0.1/32 trust
+        host all all ::1/128 trust
+        host all all 172.16.0.0/12 md5
+      '';
     };
     pgadmin = {
       enable = true;
