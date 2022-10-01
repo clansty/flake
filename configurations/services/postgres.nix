43316dd9d9f464161c06efe6090ee34fdb147de1
Clansty <i@gao4.pw>
2022-10-01 17:20:05 +0800

    add pg hba for zerotier

--- configurations/services/postgres.nix
+++ configurations/services/postgres.nix
@@ -11,6 +11,7 @@
         host all all 127.0.0.1/32 trust
         host all all ::1/128 trust
         host all all 172.16.0.0/12 md5
+        host all all 192.168.194.0/24 md5
       '';
     };
     # pgadmin = {
