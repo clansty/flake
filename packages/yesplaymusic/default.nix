3f23914d5d14e0ba1a5c8c3914fd43fdbd53614c
Clansty <i@gao4.pw>
2022-10-03 17:02:55 +0800

    update yesplaymusic

--- packages/yesplaymusic/default.nix
+++ packages/yesplaymusic/default.nix
@@ -2,50 +2,57 @@
 , fetchurl
 , electron
 , lib
-, makeWrapper
 , makeDesktopItem
-, copyDesktopItems
+, darwinHelper
+, nodePackages
 , ...
 } @ args:
 
-stdenv.mkDerivation rec {
-  pname = "yesplaymusic";
-  version = "0.4.5";
-  src = fetchurl {
-    url = "https://github.com/qier222/YesPlayMusic/releases/download/v${version}/YesPlayMusic-${version}.pacman";
-    sha256 = "d7xHCTz1+VT48I/iLpGp0NwtjYQU+FwJISbtufvsJuU=";
-  };
+let
+  app = stdenv.mkDerivation rec {
+    pname = "yesplaymusic";
+    version = "0.4.5";
+    src =
+      if stdenv.isLinux then
+        fetchurl
+          {
+            url = "https://github.com/qier222/YesPlayMusic/releases/download/v${version}/YesPlayMusic-${version}.pacman";
+            sha256 = "d7xHCTz1+VT48I/iLpGp0NwtjYQU+FwJISbtufvsJuU=";
+          }
+      else
+        darwinHelper.fetchdmg {
+          url = "https://github.com/qier222/YesPlayMusic/releases/download/v${version}/YesPlayMusic-mac-${version}-universal.dmg";
+          sha256 = "1fjx0vmlvyi0i44p0vv0328rzpbsarfzsfyg3lag4vf3s9krr8a6";
+        };
 
-  # 解压 DEB 包
-  unpackPhase = ''
-    tar -xf ${src}
-  '';
+    # 解压 DEB 包
+    unpackPhase =
+      if stdenv.isLinux then ''
+        tar -xf ${src}
+      '' else "";
 
-  nativeBuildInputs = [ copyDesktopItems makeWrapper ];
-
-  installPhase = ''
-    runHook preInstall
+    installPhase = ''
+      runHook preInstall
     
-    mkdir -p $out/bin
+      ${nodePackages.asar}/bin/asar e "${if stdenv.isDarwin then "YesPlayMusic.app/Contents" else "opt/YesPlayMusic"}/resources/app.asar" "$out"
 
-    cp -r usr/share $out/share
-    rm -rf "$out/share/applications"
-    install -Dm644 "opt/YesPlayMusic/resources/app.asar" "$out/app.asar"
-
-    makeWrapper ${electron}/bin/electron $out/bin/${pname} \
-      --add-flags "$out/app.asar"
-
-    runHook postInstall
-  '';
-
-  desktopItems = [
-    (makeDesktopItem {
-      name = "yesplaymusic";
-      exec = "yesplaymusic";
-      comment = "A third party music application for Netease Music.";
-      desktopName = "YesPlayMusic";
-      categories = [ "AudioVideo" "Audio" "Music" ];
-      icon = "yesplaymusic";
-    })
-  ];
-}
+      runHook postInstall
+    '';
+  };
+  icon = ./yesplaymusic.png;
+  exec = "${electron}/bin/electron ${app}";
+in
+if stdenv.isLinux then
+  makeDesktopItem
+  {
+    inherit icon exec;
+    name = "yesplaymusic";
+    comment = "A third party music application for Netease Music.";
+    desktopName = "YesPlayMusic";
+    categories = [ "AudioVideo" "Audio" "Music" ];
+  }
+else
+  darwinHelper.wrapApplication {
+    name = "YesPlayMusic";
+    inherit icon exec;
+  }
