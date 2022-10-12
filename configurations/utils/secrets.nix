713bfc0c7d3048a88e8db001af51a606672851ba
Clansty <i@gao4.pw>
2022-10-12 14:42:09 +0800

    update q2tg and superstar

--- configurations/utils/secrets.nix
+++ configurations/utils/secrets.nix
@@ -1,4 +1,4 @@
 import (builtins.fetchGit {
   url = "ssh://git@github.com/clansty/nix-secrets.git";
-  rev = "1cf571086c503a696a157666cbd07c6f50c969b8";
+  rev = "f9be58393f66107524e513d7db97bad818539f82";
 })
