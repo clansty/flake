33d845727edea030076e88d794d24a95419b9e56
Clansty <i@gao4.pw>
2023-01-01 18:26:50 +0800

    fix

--- configurations/services/pkgbuilder-nfs.nix
+++ configurations/services/pkgbuilder-nfs.nix
@@ -1,17 +1,17 @@
 {
   fileSystems = {
     "/pkgbuilder-work" = {
-      device = "172.16.0.70:/mnt/data/pkgbuilder-work";
+      device = "172.16.0.71:/data/pkgbuilder-work";
       fsType = "nfs";
       options = [ "nfsvers=4.2" ];
     };
     "/pkgbuilder-work/logs" = {
-      device = "172.16.0.70:/mnt/data/pkgbuilder-work/logs";
+      device = "172.16.0.71:/data/pkgbuilder-work/logs";
       fsType = "nfs";
       options = [ "nfsvers=4.2" ];
     };
     "/pkgbuilder-work/mirrors/archlinuxarm" = {
-      device = "172.16.0.70:/mnt/data/mirrors/archlinuxarm";
+      device = "172.16.0.71:/data/mirrors/archlinuxarm";
       fsType = "nfs";
       options = [ "nfsvers=4.2" ];
     };
