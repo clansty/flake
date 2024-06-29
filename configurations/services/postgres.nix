88db8512f190584e5ff3a57575138e30282a0ce3
Clansty <i@gao4.pw>
2024-06-29 22:52:52 +0800

    pg update

--- configurations/services/postgres.nix
+++ configurations/services/postgres.nix
@@ -4,20 +4,24 @@
   services = {
     postgresql = {
       enable = true;
-      package = pkgs.postgresql_14;
+      package = pkgs.postgresql_14_jit;
+      enableJIT = true;
       enableTCPIP = true;
       authentication = ''
         local all all trust
         host all all 127.0.0.1/32 trust
         host all all ::1/128 trust
         host all all 172.16.0.0/12 md5
-        host all all 192.168.194.0/24 md5
-        host all all 192.168.192.0/24 md5
         host all all 11.11.0.0/16 md5
       '';
       settings = {
         max_connections = 10000;
+        shared_preload_libraries = "vectors.so";
       };
+      extraPlugins = ps: with ps; [ 
+        pgroonga
+        pgvecto-rs
+      ];
     };
     pgadmin = {
       enable = true;
