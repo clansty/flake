d7b5484c8f5398cab2512af16633b06f405bf195
Clansty <i@gao4.pw>
2024-07-01 05:18:00 +0800

    builder

--- configurations/machines/builder/default.nix
+++ configurations/machines/builder/default.nix
@@ -1,6 +1,7 @@
 inputs: {
   modules = [
-    ({ modulesPath, ... }: modulesPath + "/virtualisation/proxmox-lxc.nix")
+    ({ modulesPath, ... }: { imports = [ (modulesPath + "/virtualisation/proxmox-lxc.nix") ]; })
     ../../services/remote-builder.nix
+    inputs.nixos-generators.nixosModules.all-formats
   ];
 }
